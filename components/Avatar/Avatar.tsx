e4885738b1097cbebefc100a477ec005b767bd06
andrea-gutierrez <andreguti93@gmail.com>
2024-05-07 23:49:27 +0800

    implemented correction of the code for the Avatar component

--- components/Avatar/Avatar.tsx
+++ components/Avatar/Avatar.tsx
@@ -5,6 +5,13 @@ interface AvatarProps {
   name?: string | null;
 }
 
+const getInitialsName = (name?: string | null): string => {
+  if(!name) return '';
+
+  const [firstName, middleName] = name.split(' ');
+  return `${firstName.at(0)}${middleName.at(0)}` ?? '';
+}
+
 export const Avatar: FC<AvatarProps> = ({ name }) => (
-  <div className={styles.avatar}>AV</div>
+  <div className={styles.avatar}>{getInitialsName(name)}</div>
 );
